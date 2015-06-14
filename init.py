6cfd581ec3565ec76d34ebd0f5143578dd507f7d
Marcelo Reyna <Lepozepo@gmail.com>
2015-06-14 14:06:25 -0700

    First approach to python using flask
    While it feels well documented, the flask repo doesn’t seem very
    active. I can’t help but compare it with Meteor’s much more effective
    and secure pub/sub structure when it comes to handling data.

---
+++ init.py
@@ -0,0 +1,60 @@
+# Equivalent to NodeJS Require
+import sqlite3
+from flask import Flask, render_template, request, g, url_for, redirect #g is for db
+from contextlib import closing
+
+# DB Config
+DATABASE = '/tmp/test.db'
+DEBUG = True
+SECRET_KEY = 'devkey'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+# Startup and namespace
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# DB Startup
+def init_db():
+	with closing(connect_db()) as db:
+		with app.open_resource('schema.sql', mode='r') as f:
+			db.cursor().executescript(f.read())
+		db.commit()
+
+def connect_db():
+	return sqlite3.connect(app.config['DATABASE'])
+
+# Handle Requests via Router Hooks?
+@app.before_request
+def before_request():
+	g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+	db = getattr(g, 'db', None)
+	if db is not None:
+		db.close()
+
+# Routes
+@app.route("/")
+def landing():
+	cur = g.db.execute('select title, text from entries order by id desc') # SQL commands >_<. Hopefully the ORM is good for complex queries
+	entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+
+	return render_template("landing.html",entries=entries)
+
+@app.route('/add', methods=['POST']) # Or we can POST old-school with AJAX
+def create_entry():
+	g.db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
+	g.db.commit()
+	return redirect(url_for('landing'))
+
+# @app.route("/hello/")
+# @app.route("/hello/<name>")
+# def hello(name=None):
+# 	return render_template("hello.html",name=name)
+
+if __name__ == '__main__':
+	app.run()
+
+
---
+++ schema.sql
@@ -0,0 +1,9 @@
+-- Entries Collection
+
+drop table if exists entries;
+create table entries (
+  id integer primary key autoincrement,
+  title text not null,
+  text text not null
+);
+
