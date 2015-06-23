fa27256c760001cd4feafc5e7604234b272926ea
Air <Air@Air.local>
2015-06-23 13:27:33 -0700

    Now compiles coffee and sass

--- init.py
+++ init.py
@@ -14,6 +14,55 @@ PASSWORD = 'default'
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+# Compilers
+if app.debug:
+	import os
+	import codecs
+	import coffeescript
+
+	# pip install git+https://github.com/imiric/flask-sass.git#egg=flask-sass
+	from scss import Scss
+
+	for root, sub_directories, files in os.walk("templates/"):
+		for file in files:
+			# COFFEESCRIPT
+			if file.endswith(".coffee"):
+				source_path = root + "/" + file
+				destination_directory = "static/js/" + root.replace("templates/","")
+				destination_path = destination_directory + "/" + file.replace(".coffee",".js")
+
+				# Create directories if they don't exist
+				if not os.path.exists(destination_directory):
+					os.makedirs(destination_directory)
+
+				source_data = codecs.open(source_path,"r",encoding="utf-8").read()
+				compiled_data = coffeescript.compile(source_data)
+
+				destination_data = codecs.open(destination_path,"w",encoding="utf-8")
+				destination_data.write(compiled_data)
+				destination_data.close()
+
+			# SCSS
+			if file.endswith(".scss"):
+				scss = Scss()
+
+				source_path = root + "/" + file
+				destination_directory = "static/css/" + root.replace("templates/","")
+				destination_path = destination_directory + "/" + file.replace(".scss",".css")
+
+				# Create directories if they don't exist
+				if not os.path.exists(destination_directory):
+					os.makedirs(destination_directory)
+
+				source_data = codecs.open(source_path,"r",encoding="utf-8").read()
+				compiled_data = scss.compile(source_data)
+
+				destination_data = codecs.open(destination_path,"w",encoding="utf-8")
+				destination_data.write(compiled_data)
+				destination_data.close()
+
+
+
 # DB Startup
 def init_db():
 	with closing(connect_db()) as db:
@@ -41,7 +90,7 @@ def landing():
 	cur = g.db.execute('select title, text from entries order by id desc') # SQL commands >_<. Hopefully the ORM is good for complex queries
 	entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
 
-	return render_template("landing.html",entries=entries)
+	return render_template("landing/landing.html",entries=entries)
 
 @app.route('/add', methods=['POST']) # Or we can POST old-school with AJAX
 def create_entry():
